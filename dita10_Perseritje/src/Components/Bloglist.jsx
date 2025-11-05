70b8ae38d419342a4af975685e89a17edbf9aba7
Lis257 <lisarifi04@gmail.com>
2025-11-05 17:17:21 +0100

    perseritja me detyra

---
+++ dita10_Perseritje/src/Components/Bloglist.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function BlogList(props) {
+
+    const list = props.list
+    const title = props.title
+    const deleteButton = props.deleteButton
+    const viewButton = props.viewButton
+  return (
+    <>
+    <div className="blog-list">
+        <h2>{ title }</h2>
+        <table border={1} cellPadding={10} cellSpacing={0}>
+            <tr>
+                <th>Emri</th>
+                <th>Img</th>
+                <th>Description</th>
+                <th>Veprimet</th>
+            </tr>
+        { list.map( (item) => (
+            <tr key={item.id}>
+                <td>{item.title}</td>
+                <td><img src={item.img} alt={item.alt} className="product-image" style={{ width: '100px', height: 'auto', borderRadius: '6px' }} /></td>
+                <td>{item.description}</td>
+                <td>
+                   <button onClick={() => props.deleteButton(item.id)}>Delete</button>
+                   <button>Edit</button>
+                   <button onClick={() => props.viewButton(item.id)}>View</button>
+                </td>
+            </tr>
+        ))
+        }
+        </table>
+    </div>
+    </>
+  )
+}
+
+export default BlogList
--- dita10_Perseritje/src/Components/Product.jsx
+++ dita10_Perseritje/src/Components/Product.jsx
@@ -1,52 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
+import Bloglist from './Bloglist'
+import usefetch from './usefetch'
+function Product() {
+    const {
+    list,
+    error,
+    loading
+    } = usefetch("http://localhost:3000/list");
+    const deletebutton = (id) => {
+        const newlist = list.filter(list => list.id != id);
+        setList(newlist);
+        
+    }
+    const view = (id) => {
+            const newlist2 = list.filter(list => list.id == id);
+            setList(newlist2);
+        }
 
-const data = {
-  list: [
-    { id: 1, name: 'Lis', age: 20, city: 'Tirana' },
-    { id: 2, name: 'Fazli', age: 21, city: 'Shkodra' },
-    { id: 3, name: 'Albatris', age: 22, city: 'Durres' },
-    { id: 4, name: 'Arlind', age: 23, city: 'Vlora' }
-  ]
-};
-
-function DataTable() {
+       
   return (
-    <div style={{ padding: '20px' }}>
-      <h2>User List</h2>
-      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-        <thead>
-          <tr>
-            <th style={thStyle}>ID</th>
-            <th style={thStyle}>Name</th>
-            <th style={thStyle}>Age</th>
-            <th style={thStyle}>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.list.map((user) => (
-            <tr key={user.id}>
-              <td style={tdStyle}>{user.id}</td>
-              <td style={tdStyle}>{user.name}</td>
-              <td style={tdStyle}>{user.age}</td>
-              <td style={tdStyle}>{user.city}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    <>
+      <h1>Home</h1>
+      {list && <Bloglist list={list} title="User List" deletebutton={deletebutton} view={view} error={error} />}
+        {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {loading && <p>Loading...</p>}
+    </>
+  )
 }
 
-const thStyle = {
-  border: '1px solid #ccc',
-  padding: '10px',
-  backgroundColor: '#f2f2f2',
-  textAlign: 'left'
-};
-
-const tdStyle = {
-  border: '1px solid #ccc',
-  padding: '10px'
-};
-
-export default DataTable;
+export default Product
